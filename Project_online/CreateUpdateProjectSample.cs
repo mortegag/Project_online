2e164bc9a7e104e828e2e10e8552721a39e05166
Moises Ortega <mortegag@hotmail.com>
2019-08-25 21:29:13 -0500

    comparacion

---
+++ Project_online/CreateUpdateProjectSample.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using csom = Microsoft.ProjectServer.Client;
+
+namespace CreateUpdateProjectSample
+{
+    partial class CreateUpdateProjectSample
+    {
+
+        // https://aigpanama.sharepoint.com/sites/Proyectos-TI/
+        public static string pwaInstanceUrl = "https://aigpanama.sharepoint.com/sites/Proyectos-TI";         // your pwa url
+        private static csom.ProjectContext context;
+        const int DEFAULTTIMEOUTSECONDS = 300;
+
+        private static string projectName = "DTR-2018005-AIG-KanbanMaestrodeProyecto";
+        private static string localResourceName = "Javier Garrido";
+        private static string taskName = "Confeccionar TDR";
+
+        private static string projectCFName = "DTR-2018005-AIG-KanbanMaestrodeProyecto";
+        private static string resourceCFName = "Javier Garrido";
+        private static string taskCFName = "Confeccionar TDR";
+
+        static void Main(string[] args)
+        {
+            //CreateProjectWithTaskAndAssignment();
+            //context = GetContext(pwaInstanceUrl);
+            ReadAndUpdateProject();
+            UpdateCustomFieldValues();
+        }
+
+        #region Utility functions
+        /// <summary>
+        /// Log to Console the job state for queued jobs
+        /// </summary>
+        /// <param name="jobState">csom jobstate</param>
+        /// <param name="jobDescription">job description</param>
+        private static void JobStateLog(csom.JobState jobState, string jobDescription)
+        {
+            switch (jobState)
+            {
+                case csom.JobState.Success:
+                    Console.WriteLine(jobDescription + " is successfully done.");
+                    break;
+                case csom.JobState.ReadyForProcessing:
+                case csom.JobState.Processing:
+                case csom.JobState.ProcessingDeferred:
+                    Console.WriteLine(jobDescription + " is taking longer than usual.");
+                    break;
+                case csom.JobState.Failed:
+                case csom.JobState.FailedNotBlocking:
+                case csom.JobState.CorrelationBlocked:
+                    Console.WriteLine(jobDescription + " failed. The job is in state: " + jobState);
+                    break;
+                default:
+                    Console.WriteLine("Unkown error, job is in state " + jobState);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Get Publish project by name
+        /// </summary>
+        /// <param name="name">the name of the project</param>
+        /// <param name="context">csom context</param>
+        /// <returns></returns>
+        private static csom.PublishedProject GetProjectByName(string name, csom.ProjectContext context)
+        {
+            IEnumerable<csom.PublishedProject> projs = context.LoadQuery(context.Projects.Where(p => p.Name == name));
+            context.ExecuteQuery();
+
+            if (!projs.Any())       // no project found
+            {
+                return null;
+            }
+            return projs.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Get csom ProjectContext by letting user type in username and password
+        /// </summary>
+        /// <param name="url">pwa website url string</param>
+        /// <returns></returns>
+        private static csom.ProjectContext GetContext(string url)
+        {
+            csom.ProjectContext context = new csom.ProjectContext(url);
+            string userName, passWord;
+
+            //Console.WriteLine("Please enter your username for PWA");
+            userName = "mortega@innovacion.gob.pa"; //Console.ReadLine();
+            //Console.WriteLine("Please enter your password for PWA");
+            passWord = "Coco.1961";//Console.ReadLine();
+
+            NetworkCredential netcred = new NetworkCredential(userName, passWord);
+            SharePointOnlineCredentials orgIDCredential = new SharePointOnlineCredentials(netcred.UserName, netcred.SecurePassword);
+            context.Credentials = orgIDCredential;
+
+            return context;
+        }
+
+        #endregion
+    }
+}
---
+++ Project_online/update.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.ProjectServer.Client;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_online
+{
+    class update
+    {
+
+        private static void UpdateProjectCustomField(Guid ProjectId)
+        {
+            DraftProject projCheckedOut = null;
+            try
+            {
+                Dictionary<string, object> projDict = new Dictionary<string, object>();
+        
+
+                using (ProjectContext projContext = new ProjectContext(PWAUrl))
+                {
+
+                    projContext.ExecutingWebRequest += claimsHelper.clientContext_ExecutingWebRequest;
+
+                    var PrjList = projContext.LoadQuery(projContext.Projects.Where(proj => proj.Id == ProjectId));
+                    projContext.ExecuteQuery();
+                    Guid pGuid = PrjList.First().Id;
+
+                    PublishedProject proj2Edit = PrjList.First();
+                    projCheckedOut = proj2Edit.CheckOut().IncludeCustomFields;
+                    projContext.Load(projCheckedOut);
+                    projContext.ExecuteQuery();
+
+                    var cflist = projContext.LoadQuery(projContext.CustomFields.Where(cf => cf.Name == "Testcol"));
+                    projContext.ExecuteQuery();
+                    projCheckedOut.SetCustomFieldValue(cflist.FirstOrDefault().InternalName, "Entry_c8f0abff70f5e51180cc00155dd45b0a");
+                
+                    QueueJob qJob = projCheckedOut.Publish(true);
+                    JobState jobState = projContext.WaitForQueue(qJob, 70);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
