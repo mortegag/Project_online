871c4c751d3ea49752545710c4cd349d610f1a6f
Moises Ortega <mortega@innovacion.gob.pa>
2019-09-04 11:08:01 -0500

    inicio de lectura de nativos y personalizados

--- Project_online/Program.cs
+++ Project_online/Program.cs
@@ -14,15 +14,15 @@ using System.Data;
 namespace Project_online
 {
 
-         
+
     class Program
     {
-
+        const int PROJECT_BLOCK_SIZE = 20;
         const string pwaPath = "https://aigpanama.sharepoint.com/sites/Proyectos-TI/";
         const string userName = "mortega@innovacion.gob.pa";
         const string passWord = "Coco.1961";
         static csom.ProjectContext ProjectCont1;
-        
+
         MySqlConnection connect = new MySqlConnection();
         string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
         /*
@@ -37,16 +37,17 @@ namespace Project_online
         /*Listado de indices GUI de proyectos del lado de Project Online
          */
         List<string> project_gui;
-        
+        static Dictionary<String, CustomField> pwaECF = new Dictionary<string, CustomField>();
+
         static void Main(string[] args)
         {
 
             //
-            var mysql = new List<string> { "auto","moto" };
-            var project = new List<string> { "auto","moto","bici"};
-            var result = mysql.Except(project.ToList()); 
+            var mysql = new List<string> { "auto", "moto" };
+            var project = new List<string> { "auto", "moto", "bici" };
+            var result = mysql.Except(project.ToList());
             //
-            
+
             Program connection = new Program();
             //connection.conn();
             // connection.actualiza_tareas("be62971f-cfc9-e911-ab58-34f39add823a", "Avances", "03/10/2018", "30/10/2018", 55);
@@ -56,7 +57,7 @@ namespace Project_online
             // connection.Project();
             //connection.customfield();
 
-            connection.ya();
+            ListPWACustomFields();
 
 
         }
@@ -94,7 +95,170 @@ namespace Project_online
         }
 
 
-        public void ya() {
+        private static void ListPWACustomFields()
+        {
+
+            ProjectCont1 = new csom.ProjectContext(pwaPath);
+            SecureString securePassword = new SecureString();
+            foreach (char c in passWord.ToCharArray())
+            {
+                securePassword.AppendChar(c);
+            }
+            ProjectCont1.Credentials = new SharePointOnlineCredentials(userName, securePassword);
+            var creds = new SharePointOnlineCredentials(userName, securePassword);
+
+            //************************************
+            // 2. Get project list with minimal information
+            ProjectCont1.Load(ProjectCont1.Projects, qp => qp.Include(qr => qr.Id));
+            ProjectCont1.ExecuteQuery();
+
+            var allIds = ProjectCont1.Projects.Select(p => p.Id).ToArray();
+
+            int numBlocks = allIds.Length / PROJECT_BLOCK_SIZE + 1;
+
+            // Query all the child objects in blocks of PROJECT_BLOCK_SIZE
+            for (int i = 0; i < numBlocks; i++)
+            {
+                var idBlock = allIds.Skip(i * PROJECT_BLOCK_SIZE).Take(PROJECT_BLOCK_SIZE);
+                Guid[] block = new Guid[PROJECT_BLOCK_SIZE];
+                Array.Copy(idBlock.ToArray(), block, idBlock.Count());
+
+                //
+          
+                DateTime hoy = DateTime.Today;
+                DateTime ayer = hoy.AddDays(0);
+                string last = ayer.ToShortDateString();
+                // 2. Retrieve and save project basic and custom field properties in an IEnumerable collection.
+                var projBlk = ProjectCont1.LoadQuery(
+                     ProjectCont1.Projects
+                    .Where(p =>   // some elements will be Zero'd guids at the end
+                        p.Id == block[0] || p.Id == block[1] ||
+                        p.Id == block[2] || p.Id == block[3] ||
+                        p.Id == block[4] || p.Id == block[5] ||
+                        p.Id == block[6] || p.Id == block[7] ||
+                        p.Id == block[8] || p.Id == block[9] ||
+                        p.Id == block[10] || p.Id == block[11] ||
+                        p.Id == block[12] || p.Id == block[13] ||
+                        p.Id == block[14] || p.Id == block[15] ||
+                        p.Id == block[16] || p.Id == block[17] ||
+                        p.Id == block[18] || p.Id == block[19] &&  
+                        p.CreatedDate >= ayer
+                    )
+                    .Include(p => p.Id,
+                        p => p.Name,
+                        p => p.Description,
+                        p => p.StartDate,
+                        p => p.FinishDate,
+                        p => p.CreatedDate,
+                        p => p.IncludeCustomFields,
+                        p => p.IncludeCustomFields.CustomFields,
+                        P => P.IncludeCustomFields.CustomFields.IncludeWithDefaultProperties(
+                            lu => lu.LookupTable,
+                            lu => lu.LookupEntries
+                        )
+                    )
+                );
+
+                ProjectCont1.ExecuteQuery();
+
+                foreach (PublishedProject pubProj in projBlk)
+                {
+
+                    // Set up access to custom field collection of published project
+                    var projECFs = pubProj.IncludeCustomFields.CustomFields;
+
+                    // Set up access to custom field values of published project
+                    Dictionary<string, object> ECFValues = pubProj.IncludeCustomFields.FieldValues;
+
+                    Console.WriteLine("Name:\t{0}", pubProj.Name);
+                    Console.WriteLine("Id:\t{0}", pubProj.Id);
+                    Console.WriteLine("Description:\t{0}", pubProj.Description);
+                    Console.WriteLine("Create Date:\t{0}", pubProj.CreatedDate);
+                    Console.WriteLine("ToDay:\t{0}", ayer);
+                    Console.WriteLine("ECF count: {0}\n", ECFValues.Count);
+
+                    Console.WriteLine("\n\tType\t   Name\t\t       L.UP   Value                  Description");
+                    Console.WriteLine("\t--------   ----------------    ----   --------------------   -----------");
+
+                    foreach (CustomField cf in projECFs)
+                    {
+
+                        // 3A. Distinguish CF values that are simple from those that use entries in lookup tables.
+                        if (!cf.LookupTable.ServerObjectIsNull.HasValue ||
+                                            (cf.LookupTable.ServerObjectIsNull.HasValue && cf.LookupTable.ServerObjectIsNull.Value))
+                        {
+                            if (ECFValues[cf.InternalName] == null)
+                            {   // 3B. Partial implementation. Not usable.
+                                String textValue = "is not set";
+                                Console.WriteLine("\t{0, -8}   {1, -20}        ***{2}",
+                                    cf.FieldType, cf.Name, textValue);
+                            }
+                            else   // 3C. Simple, friendly value for the user
+                            {
+                                // CustomFieldType is a CSOM enumeration of ECF types.
+                                switch (cf.FieldType)
+                                {
+
+                                    case CustomFieldType.COST:
+                                        decimal costValue = (decimal)ECFValues[cf.InternalName];
+                                        Console.WriteLine("\t{0, -8}   {1, -20}        {2, -22}",
+                                            cf.FieldType, cf.Name, costValue.ToString("C"));
+                                        break;
+
+                                    case CustomFieldType.DATE:
+                                    case CustomFieldType.FINISHDATE:
+                                    case CustomFieldType.DURATION:
+                                    case CustomFieldType.FLAG:
+                                    case CustomFieldType.NUMBER:
+                                    case CustomFieldType.TEXT:
+                                        Console.WriteLine("\t{0, -8}   {1, -20}        {2, -22}",
+                                            cf.FieldType, cf.Name, ECFValues[cf.InternalName]);
+                                        break;
+
+                                }
+
+                            }
+                        }
+                        else         //3D. The ECF uses a Lookup table to store the values.
+                        {
+                            Console.Write("\t{0, -8}   {1, -20}", cf.FieldType, cf.Name);
+
+                            String[] entries = (String[])ECFValues[cf.InternalName];
+
+                            foreach (String entry in entries)
+                            {
+                                var luEntry = ProjectCont1.LoadQuery(cf.LookupTable.Entries
+                                        .Where(e => e.InternalName == entry));
+
+                                ProjectCont1.ExecuteQuery();
+
+                                Console.WriteLine(" Yes    {0, -22}  {1}", luEntry.First().FullValue, luEntry.First().Description);
+                            }
+                        }
+
+
+                    }
+
+                    Console.WriteLine("     ------------------------------------------------------------------------\n");
+
+                }
+
+            }
+
+                    Console.Write("\nPress any key to exit: ");
+                        Console.ReadKey(false);
+
+        }    //end of using
+
+
+
+
+
+
+     
+
+
+        private void ya() {
 
 
             ProjectCont1 = new csom.ProjectContext(pwaPath);
@@ -107,7 +271,7 @@ namespace Project_online
             var creds = new SharePointOnlineCredentials(userName, securePassword);
             int j = 0;
             ProjectCont1.Load(ProjectCont1.Projects,
-                   c => c.IncludeWithDefaultProperties
+                   c=>c.IncludeWithDefaultProperties
                    (
                    pr => pr.CustomFields,
                    pr => pr.IncludeCustomFields,
@@ -120,20 +284,21 @@ namespace Project_online
                 {
                     foreach (var cust in item.IncludeCustomFields.FieldValues)
                     {
-                        string customfieldID = cust.Key;
-                        
+                        string customfieldID = cust.Key;                        
                         string CsutomfieldValue = cust.Value.ToString();
-                        Console.WriteLine("\n{0}. {1} \n", j++, customfieldID, CsutomfieldValue);
+                        Console.WriteLine("\n{0}. {1} \t{2} \n", j++, customfieldID, CsutomfieldValue);
+                        
 
+                       
                     }
                 }
             }
 
 
         }
+                     
 
-
-        public void conn()
+        private void conn()
         {
 
             ProjectCont1 = new csom.ProjectContext(pwaPath);
